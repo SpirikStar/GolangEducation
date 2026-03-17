3b1d374c830ef34f9cb558d8a72280130f36f8ba
Alexey Spiridonov <92844309+SpirikStar@users.noreply.github.com>
2026-03-17 12:02:46 +0300

    refactor

--- edu/mokit/oibas-9-24/17.03.2026/main.go
+++ edu/mokit/oibas-9-24/17.03.2026/main.go
@@ -1,8 +1,24 @@
 package main
 
+type Config struct {
+	Count uint `json:"count"`
+}
+
+func readFile() {
+
+}
+
+func writeFile() {}
+
 func main() {
+	var waitGroup sync.WaitGroup
+	var mutex sync.Mutex
+
+	for i := 0; i < 5; i++ {
+
+	}
 	// TODO: edu/mokit/oibas-9-24/17.03/main.go
 	// TODO: edu/mokit/oibas-9-24/10.03/main.go
-	
+
 	// TODO: edu/mokit/oibas-9-24/17.03.2026/Задание.png
 }
