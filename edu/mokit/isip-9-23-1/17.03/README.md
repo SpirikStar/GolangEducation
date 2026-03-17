28eb41745a59a213d5e619b76059707b8cec168d
Alexey Spiridonov <92844309+SpirikStar@users.noreply.github.com>
2026-03-17 14:30:46 +0300

    refactor: db

---
+++ edu/mokit/isip-9-23-1/17.03/README.md
@@ -0,0 +1,12 @@
+```cmd
+go mod init lesson
+```
+MAC OS, Linux
+```cmd
+go get github.com/mattn/go-sqlite3
+```
+
+windows 10 VS 11
+```cmd
+go get "modernc.org/sqlite"
+```
--- edu/mokit/isip-9-23-1/17.03/main.go
+++ edu/mokit/isip-9-23-1/17.03/main.go
@@ -1,5 +1,46 @@
 package main
 
-func main() {
+import (
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
+)
 
+type User struct {
+	ID        uint
+	FirstName string
+	LastName  string
+	Email     string
+	City      string
+	Age       uint
+	CreatedAt string
+}
+
+type Message struct {
+	ID         uint
+	SenderID   uint
+	ReceiverID uint
+	Text       string
+	SentAt     string
+	IsRead     bool
+}
+
+func openDB(path string) *sql.DB {
+	db, err := sql.Open("sqlite3", path)
+	if err != nil {
+		log.Fatalln("Ошибка:", err)
+	}
+	return db
+}
+func main() {
+	db := openDB("messenger.db")
+	defer db.Close()
+
+	var user User
+	db.QueryRow("SELECT * FROM users WHERE id = ?", 1).Scan(
+		&user.ID, &user.FirstName, &user.LastName,
+		&user.Email, &user.City, &user.Age,
+		&user.CreatedAt,
+	)
+	log.Println(user)
 }
