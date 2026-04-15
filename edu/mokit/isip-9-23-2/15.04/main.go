65037f8256f17d91677bd59a7a593b2677bb0a7b
Alexey Spiridonov <92844309+SpirikStar@users.noreply.github.com>
2026-04-15 10:01:28 +0300

    add work

--- edu/mokit/isip-9-23-2/15.04/main.go
+++ edu/mokit/isip-9-23-2/15.04/main.go
@@ -3,9 +3,24 @@
 // TODO: go run .
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func init() {
+	err := os.Mkdir("avatars", 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
+	// TODO: Создать функцию downloadAvatar
+	// TODO: параметры: code, id
+	// TODO: Сохранения изображения в папку avatars
+	// TODO: Название файла индентичное id
 	avatar := getAvatar(1)
 	fmt.Println(avatar)
 }
